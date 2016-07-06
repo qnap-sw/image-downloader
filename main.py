17f176b8a6ef76c691049c98a70b911744718b74
LouisLiu79179 <louisliu@qnap.com>
2016-07-06 19:26:24 +0800

    Add files via upload
    This version provide user download Google search image by input csv file 
    TODO: check Action # comment command
               the following searchAPI source code need to be modified 
               Query: *-hello
               color: transparent

---
+++ main.py
@@ -0,0 +1,34 @@
+import csv 
+import sys
+
+# resolved csv
+# row 1 is key, row 2 is options, row 3 4... is value
+csv_name = raw_input("please input csv name: ")
+csv_file = open(csv_name)
+csv_reader = csv.reader(csv_file)
+csv_data = list(csv_reader)
+
+from Crawler import PictureCrawler
+
+class mainApp(object):
+
+	# need to check action
+	for task in csv_data[2:]:
+	    crawler = PictureCrawler()
+	    crawler.Action = task[0]
+	    crawler.SearchEngine = task[1] 
+        crawler.Query = task[2]
+	    crawler.Limit = task[3]
+ 	    crawler.ImageSize = task[4]
+	    crawler.AspectRatio = task[5]
+	    crawler.Color = task[6]
+	    crawler.ImageType = task[7]
+	    crawler.FileType = task[8]
+	    if task[9] != "":
+	    	crawler.DownloadPath = task[9]
+	    crawler.handle_download(crawler.handle_search())
+	
+
+
+
+
