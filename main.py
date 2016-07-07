1c229dc638cb895994e484386ab12db17ac1e031
LouisLiu79179 <louisliu@qnap.com>
2016-07-07 18:12:10 +0800

    simple check action

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv 
 import sys
 
-# resolved csv
+#resolved csv
 # row 1 is key, row 2 is options, row 3 4... is value
 csv_name = raw_input("please input csv name: ")
 csv_file = open(csv_name)
@@ -12,22 +12,24 @@ from Crawler import PictureCrawler
 
 class mainApp(object):
 
-	# need to check action
-	for task in csv_data[2:]:
-	    crawler = PictureCrawler()
+    # set search option
+	for task in csv_data[3:]:crawler = PictureCrawler()
 	    crawler.Action = task[0]
 	    crawler.SearchEngine = task[1] 
-        crawler.Query = task[2]
+	    crawler.Query = task[2]
 	    crawler.Limit = task[3]
- 	    crawler.ImageSize = task[4]
+	    crawler.ImageSize = task[4]
 	    crawler.AspectRatio = task[5]
 	    crawler.Color = task[6]
 	    crawler.ImageType = task[7]
 	    crawler.FileType = task[8]
+
 	    if task[9] != "":
 	    	crawler.DownloadPath = task[9]
-	    crawler.handle_download(crawler.handle_search())
-	
+            if crawler.Action == 'image.query.download':
+            	crawler.handle_download(crawler.handle_search())
+            else:
+                print crawler.Action + 'is inactive'
 
 
 
